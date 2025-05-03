f90157dd54bfc77a15ae3520580667efc34b8a43
yash garg <yg292001@gmail.com>
2025-05-03 11:53:48 +0530

    backend fix

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,9 +4,9 @@ const UserSchema = new mongoose.Schema({
     googleId: { type: String, required: true, unique: true },
     name: { type: String, required: true },
     firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
+    lastName: { type: String },
     email: { type: String, required: true, unique: true },
-    picture: { type: String, required: true },
+    picture: { type: String },
 }, { timestamps: true });
 
 const User = mongoose.model("User", UserSchema);
